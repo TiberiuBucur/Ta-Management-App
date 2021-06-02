79226214f741179c8203d7af0b4e1c84086e9e8f
rab19 <radu.bonea19@imperial.ac.uk>
2021-06-02 13:41:36 +0300

    Add boilerplate code

---
+++ src/server/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 5000;
+
+app.use(express.static(path.resolve(__dirname, "../client/build")));
+
+// app.get("*", (_, res) => {
+//   res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+// })
+
+app.listen(PORT);
+console.log(`Listening on port ${PORT}`);
