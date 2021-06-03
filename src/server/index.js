17e831243f12c64aa4ad4daf62bd0857526ed3e9
Mihnea Gogu <mrg119@ic.ac.uk>
2021-06-03 17:57:30 +0300

    [all] added connection from Docker to heroku's postgres

--- src/server/index.js
+++ src/server/index.js
@@ -4,9 +4,40 @@ const { Pool, Client } = require('pg');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const dbUrl = process.env.DATABASE_URL || "SOMETHING SOMETHING";
+console.log(dbUrl);
 
 const name = 'ag1319';
 const addOn = '5';
+const devMode = false;
+
+const pool;
+if (devMode) {
+
+    pool = new Pool({
+        user: 'postgres',
+        host: 'localhost',
+        database: 'testdb',
+        password: 'fghijbon8976',
+        port: 5432
+    });
+} else {
+    pool = new Pool({
+        connectionString: dbUrl
+    });
+}
+
+
+pool.query('SELECT NOW() as now')
+    .then(res => console.log(res))
+    .catch(err => console.log(err, "OMG DIDNT WORK---------------------------------------------------------------"));
+
+// pool.query('SELECT * FROM users;')
+//     .then(res => {
+//         console.log(res);
+//     })
+//     .catch(err => { console.log(err) })
+//     .finally(() => pool.end());
 
 app.use(express.static(path.resolve(__dirname, "../client/build")));
 app.use(express.json())
@@ -20,18 +51,3 @@ app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
 
-const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'testdb',
-    password: 'fghijbon8976',
-    port: 5432
-});
-
-
-pool.query('SELECT * FROM users;')
-    .then(res => {
-        console.log(res);
-    })
-    .catch(err => { console.log(err) })
-    .finally(() => pool.end());
