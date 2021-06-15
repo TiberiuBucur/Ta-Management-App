9c7966f503453241719c01d7155c561f84dd0b95
andreigramescu <andrei.gramescu@gmail.com>
2021-06-15 09:40:21 +0300

    Add compute schedule button for coordinator

--- src/client/src/coord/Dashboard.tsx
+++ src/client/src/coord/Dashboard.tsx
@@ -1,5 +1,26 @@
-import React from "react"
+import React from "react";
 
-const Dashboard = () => <div>Loading dashboard...  (won't stop. TODO)</div>
+const Dashboard = () => {
+  return (
+    <div className="dashboard">
+      <button
+        className="compute-schedule-bttn"
+        onClick={() => {
+          fetch("/computesched", {
+            method: "POST",
+            mode: "same-origin",
+            cache: "no-cache",
+            credentials: "same-origin",
+          })
+            .then(res => res.json())
+            .then(({ msg }) => alert(msg))
+            .catch(err => alert("Something went wrong"));
+        }}
+      >
+        Compute Schedule
+      </button>
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
